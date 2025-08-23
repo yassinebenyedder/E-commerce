7669d06ddfaa0528513d1f456ef3462bf2ceec2b
Yassine Ben Yedder <benyedderyassin7@gmail.com>
2025-08-23 01:09:14 +0100

    add analytics vercel system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -94,6 +95,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </CartProvider>
+        <Analytics />
       </body>
     </html>
   );
